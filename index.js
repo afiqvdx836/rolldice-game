db2538f74a785552a2b503f5105bf645c1abbf6e
Afiq <afiqvdx20@gmail.com>
2023-01-24 12:12:11 +0800

    Add player 2 function

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ var randomImageSource = "images/" + randomImageDice;
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageDice2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomImageDice2;
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
